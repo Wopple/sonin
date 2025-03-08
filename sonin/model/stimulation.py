bbfd2d13b6542f65b8191a7176f04d5f2f9469e5
Daniel Tashjian <thewopple@gmail.com>
2025-03-08 12:37:09 -0500

    implement SnapBack

---
+++ sonin/model/stimulation.py
@@ -0,0 +1,24 @@
+class SnapBack:
+    def __init__(self, baseline: int = 0, restore_rate: int = 1, restore_scalar: int = 1):
+        # Baseline adjustments happen transparently at get and set time. The step
+        # math is easier with a baseline of zero.
+        self.baseline: int = baseline
+
+        self.restore_rate: int = restore_rate
+        self.restore_scalar: int = restore_scalar
+        self._value: int = baseline
+
+    @property
+    def value(self):
+        return self._value + self.baseline
+
+    @value.setter
+    def value(self, value):
+        self._value = value - self.baseline
+
+    def step(self):
+        if self._value > 0:
+            self._value = self._value * self.restore_scalar // self.restore_rate
+        elif self._value < 0:
+            positive = -self._value
+            self._value = -(positive * self.restore_scalar // self.restore_rate)
---
+++ tests/sonin/model/test_stimulation.py
@@ -0,0 +1,28 @@
+import pytest
+
+from sonin.model.stimulation import SnapBack
+
+
+@pytest.mark.parametrize(
+    'baseline, rate, scalar, initial, expected',
+    [
+        (0, 2, 1, 0, 0),
+        (0, 2, 1, 1, 0),
+        (0, 2, 1, -1, 0),
+        (-1, 2, 1, -2, -1),
+        (-2, 2, 1, -2, -2),
+        (0, 3, 2, 10, 6),
+        (3, 3, 2, 10, 7),
+        (-3, 3, 2, 10, 5),
+        (0, 3, 2, -10, -6),
+        (3, 3, 2, -10, -5),
+        (-3, 3, 2, -10, -7),
+    ]
+)
+def test_snap_back_step(baseline: int, rate: int, scalar: int, initial: int, expected: int):
+    snap_back = SnapBack(baseline, rate, scalar)
+
+    snap_back.value = initial
+    snap_back.step()
+
+    assert snap_back.value == expected
