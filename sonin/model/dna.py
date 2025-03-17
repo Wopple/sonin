ff86f3a0bd26db277ed204e6eac499d657cd2b0e
Daniel Tashjian <thewopple@gmail.com>
2025-03-16 22:39:13 -0400

    add mutable DNA

--- sonin/model/dna.py
+++ sonin/model/dna.py
@@ -57,7 +57,10 @@ class Mutator:
         self.mutagen_map = {i: m for i, m in enumerate(self.mutagens)}
         self.mutation_selector = [i for i, m in self.mutagen_map.items() for _ in range(m.occurrence_weight)]
 
-    def mutate(self, num_mutations: int):
+    def mutate(self, num_mutations: int | None = None):
+        assert num_mutations > 0
+
+        num_mutations = num_mutations or 1
         shuffle(self.mutation_selector)
         mutations = {}
 
@@ -79,38 +82,91 @@ class Dna:
         n_synapse: int = 1,
         n_dimension: int = 1,
         activation_level: int = 1,
-        max_neuron_strength: int = 2,
+        max_neuron_strength: int = 1,
         refactory_period: int = 0,
         facilitation_granularity: int = 1,
         facilitation_limit: int = 1,
     ):
+        # Lower limit on the number of neurons in the hypercube
+        self._min_neurons: UintMutagen = UintMutagen(min_neurons, 1)
+
         # Number of synapses per neuron
-        self.n_synapse: int = n_synapse
+        self._n_synapse: UintMutagen = UintMutagen(n_synapse, 1)
 
         # Number of virtual spacial dimensions in a mind
-        self.n_dimension: int = n_dimension
+        self._n_dimension: UintMutagen = UintMutagen(n_dimension, 1, 5)
 
         # Level at which a neuron activates
-        self.activation_level: int = activation_level
+        self._activation_level: UintMutagen = UintMutagen(activation_level, 1)
 
         # Neurons cannot propagate more potential than this when activating
-        self.max_neuron_strength: int = max_neuron_strength
+        self._max_neuron_strength: UintMutagen = UintMutagen(max_neuron_strength, 1)
 
         # Amount of time a neuron stays in the refactory state (min 1)
-        self.refactory_period: int = refactory_period
+        self._refactory_period: UintMutagen = UintMutagen(refactory_period)
 
-        self.facilitation_granularity: int = facilitation_granularity
-        self.facilitation_limit: int = facilitation_limit
+        self._facilitation_granularity: UintMutagen = UintMutagen(facilitation_granularity, 1)
+        self._facilitation_limit: UintMutagen = UintMutagen(facilitation_limit, 1)
 
+        self.mutator: Mutator = Mutator([
+            self._min_neurons,
+            self._n_synapse,
+            self._n_dimension,
+            self._activation_level,
+            self._max_neuron_strength,
+            self._refactory_period,
+            self._facilitation_granularity,
+            self._facilitation_limit,
+        ])
+
+        self.initialize()
+
+    def initialize(self):
         # Size of the hypercube of neurons, rounded up from the minimum number of neurons such that
         # dimension_size ^ n_dimension >= min_neurons
 
         dimension_size: int = 1
 
-        while dimension_size ** n_dimension < min_neurons:
+        while dimension_size ** self.n_dimension < self.min_neurons:
             dimension_size += 1
 
         self.dimension_size: int = dimension_size
 
         # Total number of neurons
-        self.n_neuron: int = self.dimension_size ** n_dimension
+        self.n_neuron: int = self.dimension_size ** self.n_dimension
+
+    @property
+    def min_neurons(self) -> int:
+        return self._min_neurons.value
+
+    @property
+    def n_synapse(self) -> int:
+        return self._n_synapse.value
+
+    @property
+    def n_dimension(self) -> int:
+        return self._n_dimension.value
+
+    @property
+    def activation_level(self) -> int:
+        return self._activation_level.value
+
+    @property
+    def max_neuron_strength(self) -> int:
+        return self._max_neuron_strength.value
+
+    @property
+    def refactory_period(self) -> int:
+        return self._refactory_period.value
+
+    @property
+    def facilitation_granularity(self) -> int:
+        return self._facilitation_granularity.value
+
+    @property
+    def facilitation_limit(self) -> int:
+        return self._facilitation_limit.value
+
+    def mutate(self, num_mutations: int | None = None):
+        self.mutator.mutate(num_mutations)
+        self.initialize()
